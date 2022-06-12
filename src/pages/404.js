6ae53432ba0c6a04dd65896ad5948f4dc2cdfddf
Rolv Apneseth <rolv.apneseth@gmail.com>
2022-06-12 13:53:37 +0100

    Upgraded the 404 page to look more in line with the home page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,28 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 const PageNotFound = () => {
-  return (
-    <React.Fragment>
-      <h1>Woops! Page Not Found</h1>
-    </React.Fragment>
-  )
+    return (
+        <>
+            <Layout>
+                <section style={{ textAlign: "center" }} id="intro">
+                    <h2>Uh oh!</h2>
+                    <h1>The page you are looking for could not be found.</h1>
+                    <p>
+                        If you believe this is an error please open an
+                        issue&nbsp;
+                        <a
+                            href="https://github.com/Rolv-Apneseth/portfolio/issues"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            here
+                        </a>
+                    </p>
+                </section>
+            </Layout>
+        </>
+    )
 }
 
 export default PageNotFound
