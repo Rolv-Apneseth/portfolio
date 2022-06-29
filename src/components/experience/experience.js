f08e466d24ba21257647b0a4621d916e433538a3
Rolv Apneseth <rolv.apneseth@gmail.com>
2022-06-29 22:17:06 +0100

    Minor changes - reworded work experience bullet points

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -16,9 +16,10 @@ export const experiences = [
         '<a target="_blank" rel="noreferrer" href="https://iclass-cms.com/">Bottletop Media (iClassCMS)</a>',
         "September 2021 - Present",
         [
-            "Create and maintain the front end code for school websites, and ensure sites are always backed up using Git.",
-            "Help implement unique designs and features for schools/governing bodies who are looking for something a little more interesting or aesthetically pleasing.",
-            "Create page structures on the company's CMS software for new clients' websites using a Selenium (browser automation) script I created.",
+            "Maintain and make changes to websites for existing clients, using HTML, CSS, JavaScript and VB.Net",
+            "Implement unique designs/features outside of the base templates using modern CSS and JavaScript functionality",
+            "Create page structures on the company's custom CMS software for new clients' websites using a Selenium (browser automation) script I created using Python",
+            "Maintain version control for the websites using Git",
         ]
     ),
 ]
